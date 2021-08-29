0b726f963468f960a4dc51b43c2e081dc9c21ee3
Brane Marjanovic <marjanovicbrane93@gmail.com>
2021-08-29 16:58:17 +0200

    I created a new class CustomerController.This is CONTROLLER LAYER.In this class I made method listCustomers.This method I am going to use to show list of customers.First we need to make list-customers.jsp page.

---
+++ src/main/java/com/brane/spring/controller/CustomerController.java
@@ -0,0 +1,53 @@
+package com.brane.spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.brane.spring.entity.Customer;
+import com.brane.spring.service.CustomerService;
+
+//CONTROLLER LAYER
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+	// need to inject our customer service
+	@Autowired
+	private CustomerService customerService;
+	
+	
+	//Making request mapping /list and then we will store list of objects into attribute model called customers.
+	//This model we will use in our JSP PAGE to show all data in table with for each loop.
+	@GetMapping("/list")
+	public String listCustomers(Model theModel) {
+		
+		//delegate calls from controller to service layer
+		//get customers from db
+		List<Customer> theCustomers = customerService.getCustomers();
+				
+		//add the customers to the model
+		theModel.addAttribute("customers", theCustomers);
+		
+		//we'll return list-customers.jsp
+		return "list-customers";
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
